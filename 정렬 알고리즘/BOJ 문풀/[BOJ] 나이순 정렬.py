1d40c433564b44d23f4fa9de37e2a9898859a33e
maruhxn <a23686397@gmail.com>
2022-08-02 23:29:10 +0900

    정렬 1

--- 정렬 알고리즘/BOJ 문풀/[BOJ] 나이순 정렬.py
+++ 정렬 알고리즘/BOJ 문풀/[BOJ] 나이순 정렬.py
@@ -1,5 +1,8 @@
 n = int(input())
-nodes = [tuple(input().split()) for _ in range(n)]
-arr = sorted(nodes, key=lambda x : x[0])
-for x, y in arr:
+nodes = [list(input().split()) for _ in range(n)]
+for i in range(len(nodes)):
+  nodes[i][0] = int(nodes[i][0])
+  nodes[i].append(i)
+nodes.sort(key= lambda x: (x[0], x[2]))
+for x, y, _ in nodes:
   print(x, y)
